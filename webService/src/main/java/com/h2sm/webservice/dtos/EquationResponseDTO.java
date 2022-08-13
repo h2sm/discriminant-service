045d8dfe164c9cb4dba8a6bcc1815c56a667567f
Egor Fedorenko <fedore.egor@gmail.com>
2022-08-13 19:04:39 +0300

    Works interaction with services

--- webService/src/main/java/com/h2sm/webservice/dtos/EquationResponseDTO.java
+++ webService/src/main/java/com/h2sm/webservice/dtos/EquationResponseDTO.java
@@ -1,10 +1,14 @@
 package com.h2sm.webservice.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EquationResponseDTO {
     private String formula;
     private double d;
--- webService/src/main/java/com/h2sm/webservice/service/RequestService.java
+++ webService/src/main/java/com/h2sm/webservice/service/RequestService.java
@@ -3,8 +3,10 @@ package com.h2sm.webservice.service;
 import com.h2sm.webservice.dtos.EquationRequestDTO;
 import com.h2sm.webservice.dtos.EquationResponseDTO;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.Element;
 
 import javax.xml.soap.*;
+import java.util.Iterator;
 
 @Service
 public class RequestService {
@@ -14,16 +16,13 @@ public class RequestService {
         String soapEndpointUrl = "http://localhost:8081/ws";
         String soapAction = "EquationRequest";
 
-        callSoapWebService(soapEndpointUrl, soapAction, dto);
+        return callSoapWebService(soapEndpointUrl, soapAction, dto);
 
-        return EquationResponseDTO
-                .builder()
-                .build();
     }
 
-    private void callSoapWebService(String soapEndpointUrl,
-                                    String soapAction,
-                                    EquationRequestDTO dto) {
+    private EquationResponseDTO callSoapWebService(String soapEndpointUrl,
+                                                   String soapAction,
+                                                   EquationRequestDTO dto) {
         try {
             // Create SOAP Connection
             SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
@@ -32,18 +31,18 @@ public class RequestService {
             // Send SOAP Message to SOAP Server
             SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(soapAction, dto), soapEndpointUrl);
 
-            SOAPEnvelope envelope = soapResponse.getSOAPPart().getEnvelope();
-
             // Print the SOAP Response
             System.out.println("Response SOAP Message:");
             soapResponse.writeTo(System.out);
             System.out.println();
 
             soapConnection.close();
+            return parseAnswer(soapResponse);
         } catch (Exception e) {
             System.err.println("\nError occurred while sending SOAP Request to Server!\nMake sure you have the correct endpoint URL and SOAPAction!\n");
             e.printStackTrace();
         }
+        return null;
     }
 
 
@@ -101,6 +100,56 @@ public class RequestService {
         SOAPElement cElem = soapBodyElem.addChildElement("c", myNamespace);
         cElem.addTextNode(Integer.toString(dto.getC()));
 
+    }
+
+    private EquationResponseDTO parseAnswer(SOAPMessage soapResponse) throws SOAPException {
+        var resp = new EquationResponseDTO();
+
+        var body = soapResponse.getSOAPBody();
+        Iterator replyIt = body.getChildElements();
+
+        while(replyIt.hasNext()) {
+            SOAPBodyElement replysbe = (SOAPBodyElement) replyIt.next();
+            Iterator replyIt2 = replysbe.getChildElements();
+            while (replyIt2.hasNext()){
+                var ele = (SOAPElement) replyIt2.next();
+                var name = ele.getElementName().getLocalName();
+                switch (name) {
+                    case "formula":
+                        resp.setFormula(ele.getValue());
+                        break;
+                    case "D":
+                        resp.setD(Double.parseDouble(ele.getValue()));
+                        break;
+                    case "x1":
+                        resp.setX1(Double.parseDouble(ele.getValue()));
+                        break;
+                    case "x2":
+                        resp.setX2(Double.parseDouble(ele.getValue()));
+                        break;
+                }
+                var val = ele.getValue();
+            System.out.println(val);
+            }
+            //SOAPElement sentSE = (SOAPElement) replyIt2.next();
+        }
+
+
+//        while (itr.hasNext()) {
+//            Node node = itr.next();
+//            if (node.getNodeType() == Node.ELEMENT_NODE) {
+//                Element ele = (Element) node;
+//                switch (ele.getNodeName()) {
+//                    default:
+//                        System.out.println(ele.getNodeName());
+//                        break;
+//                }
+//            } else if (node.getNodeType() == Node.TEXT_NODE) {
+//                //do nothing here most likely, as the response nearly never has mixed content type
+//                //this is just for your reference
+//            }
+//        }
+        return resp;
     }
 }
 
